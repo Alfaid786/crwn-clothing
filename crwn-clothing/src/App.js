0cc1dc4a70e6825d58df01ffea9d3df177cdb319
Alfaid786 <dhanmendra777@gmail.com>
2021-04-24 10:47:34 +0530

    React Router Section Done

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/Homepage/homepage.component.jsx";
+
+const HatsPage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1> HATS Page</h1>
+    </div>
+  );
+};
+
+const TopicList = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Topic List Page</h1>
+    </div>
+  );
+};
+
+const TopicDetail = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Topic Detail: {props.match.params.topicId}</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/shop/hats" component={HatsPage} />
+      <Route exact path="/topics" component={TopicList} />
+      <Route path="/topics/:topicId" component={TopicDetail} />
     </div>
   );
 }
