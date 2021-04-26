4f134db99689cdfdbda9addef0cb3eeeb4b348bf
Alfaid786 <dhanmendra777@gmail.com>
2021-04-26 10:38:13 +0530

    Cart items and quantity added

--- crwn-clothing/src/components/collection-item/collection-item.component.jsx
+++ crwn-clothing/src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
 import './collection-item.style.scss';
 
-const CollectionItem = ({ name, price, imageUrl}) => {
+import {addItem } from '../../redux/cart/cart.actions'
+import CustomButton from '../custom-button/custom-button.component'
+
+const CollectionItem = ({ item, addItem }) => {
+    const { name, price, imageUrl } = item;
     return (
          <div className='collection-item'>
         <div
@@ -10,10 +16,17 @@ const CollectionItem = ({ name, price, imageUrl}) => {
             <div className='collection-footer'>
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
-          </div>
+            </div>
+            <CustomButton onClick={()=> addItem(item) } inverted>Add to Cart</CustomButton>
      </div>
         
     )
 }
 
-export default CollectionItem
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null,
+    mapDispatchToProps)
+    (CollectionItem);
--- crwn-clothing/src/redux/cart/cart.actions.js
+++ crwn-clothing/src/redux/cart/cart.actions.js
@@ -1,5 +1,11 @@
+import cartActionTypes from "./cart.type";
 import CartActionTypes from "./cart.type";
 
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN,
+});
+
+export const addItem = (item) => ({
+  type: cartActionTypes.ADD_ITEM,
+  payload: item,
 });
