e2693d15f80b2a7edc8bc0d169db43671012efb5
Alfaid786 <dhanmendra777@gmail.com>
2021-04-26 15:11:50 +0530

    some update on closing all files

--- crwn-clothing/src/components/header/header.component.jsx
+++ crwn-clothing/src/components/header/header.component.jsx
@@ -4,8 +4,11 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../asset/crown.svg';
 import { auth } from '../../firebase/firebase.utils.js';
 import { connect } from 'react-redux';
+import {createStructuredSelector} from 'reselect'
 import CartIcon from '../cart-icon/cart-icon.component'
 import CartDropdown from '../cart-dropdown/cart-dropdown.component'
+import { selectCartHidden } from '../../redux/cart/cart.selectors'
+import {selectCurrentUser} from '../../redux/user/user.selector' 
 
 const Header = ({ currentUser, hidden}) => {
     return (
@@ -34,9 +37,9 @@ const Header = ({ currentUser, hidden}) => {
     )
 }
 
-const mapStateToProps = ({user:{currentUser},cart:{hidden}}) => ({
-    currentUser,
-    hidden
+const mapStateToProps = createStructuredSelector ({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 })
 
 export default connect(mapStateToProps)(Header);
